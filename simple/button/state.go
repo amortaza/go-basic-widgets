8a351a9cd5a8e986570ae6abae8a979db7bff8f7
amortaza <davidnia1@gmail.com>
2016-06-24 21:23:48 -0700

    Init

---
+++ simple/button/state.go
@@ -0,0 +1,23 @@
+package button
+
+type ButtonState struct {
+	IsHover bool
+	IsDown bool
+}
+
+var g_stateByButtonId map[string] *ButtonState
+
+func newState() *ButtonState {
+	return &ButtonState{}
+}
+
+func getOrCreateState(buttonId string) *ButtonState {
+	state, ok := g_stateByButtonId[buttonId]
+
+	if !ok {
+		state = newState()
+		g_stateByButtonId[buttonId] = state
+	}
+
+	return state
+}
